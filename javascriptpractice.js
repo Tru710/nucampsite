90452b1b5850b58d64ecefc0dc3c77e192029186
Jesus T <flareon831@gmail.com>
2020-03-23 22:34:04 -0700

    Bootstrap and JQuery Part 1

---
+++ javascriptpractice.js
@@ -0,0 +1,26 @@
+// declaring variables:
+const firstNumber = 3;
+console.log=('first number is', firstNumber)
+
+// string booleans:
+let secondNumber = 2;
+console.log('second')
+
+let sum = firstNumber + secondNumber;
+console.log('sum is', sum);
+
+// if condition is true it will run the whats inside the curly braces
+let isCool = true;
+
+if(isCool) {
+    console.log('congrats! You are cool.');
+}   else{
+        console.log('too bad, youre lame.');
+}
+
+/// declaration vs execution.
+function sayHi() {
+    console.log('hey whats up');
+}
+// execution.
+sayHi()
